6c0e3950fc60ec8047612806d2e4bbe95ae6a925
nilout <stlsniloud@gmail.com>
2021-11-14 14:40:01 +0700

    Updates

---
+++ precache-manifest.8a50008ad493911ea72bba466b99a9e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761da32d62fe131980ba4417ede6ab55",
+    "url": "/React-pf/index.html"
+  },
+  {
+    "revision": "46ba24bb95334168826e",
+    "url": "/React-pf/static/css/main.ec9bf0bb.chunk.css"
+  },
+  {
+    "revision": "229095403672d9ceb0ec",
+    "url": "/React-pf/static/js/2.df8fa722.chunk.js"
+  },
+  {
+    "revision": "a09bcd6464a11c77a5caeae57fde5da8",
+    "url": "/React-pf/static/js/2.df8fa722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ba24bb95334168826e",
+    "url": "/React-pf/static/js/main.1a29d2aa.chunk.js"
+  },
+  {
+    "revision": "1a7f1ea91510626597c9",
+    "url": "/React-pf/static/js/runtime-main.de64e9d6.js"
+  },
+  {
+    "revision": "cdd50559dcde92b7e891315050614ab9",
+    "url": "/React-pf/static/media/me.cdd50559.png"
+  }
+]);
